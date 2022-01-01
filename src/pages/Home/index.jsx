28abc13180b577ab728dcf19732949f5bc90a4ca
Robert Shaw <r.shaw1@yahoo.com>
2022-01-01 04:56:38 +0000

    additional styling for game tile

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Tile } from './Tile';
+import { NumberMemoryIcon } from '../../components/Icons/NumberMemoryIcon';
 
 export const Home = () => {
   return (
     <section>
-      <Tile title="Number Memory" desc="Remember the longest number you can." />
+      <Tile
+        icon={<NumberMemoryIcon />}
+        title="Number Memory"
+        desc="Remember the longest number you can."
+      />
     </section>
   );
 };
