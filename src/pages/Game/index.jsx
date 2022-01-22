847d00a5fa59ce12eecf368ab1f81295d87835a9
Tanya627 <tanyaocampo96@yahoo.com>
2022-01-22 03:43:41 +0000

    - Resolved #10

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,13 +1,26 @@
+/* Style */
+import './index.css';
+
 /* Node Modules */
 import React from 'react';
-import './index.css';
+import { NumberMemoryIcon } from '../../components/Icons/NumberMemoryIcon';
 
 /* Component */
 export const Game = () => {
   return (
     <div className="container">
       <section className="game">
-        <h1>Number Memory</h1>
+        <div className="game-content">
+          <div>
+            <NumberMemoryIcon />
+          </div>
+
+          <h1>Number Memory</h1>
+          <p>
+            The average person can remember 7 numbers at once. Can you do more?
+          </p>
+          <button className="start-button">Start</button>
+        </div>
       </section>
       <section className="info"></section>
     </div>
